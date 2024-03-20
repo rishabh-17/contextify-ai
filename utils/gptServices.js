dea08d2463fb9fe6575308bcee70cd1af9711bc2
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-03-20 21:22:04 +0530

    Ai services

---
+++ utils/gptServices.js
@@ -0,0 +1,59 @@
+const OpenAI = require("openai");
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const explainText = async (text) => {
+  if (text?.length > 0) {
+    const prompt = [
+      {
+        role: "user",
+        content: "explain meaning of given text",
+      },
+      {
+        role: "user",
+        content: text,
+      },
+    ];
+
+    openai.chat.completions
+      .create({
+        model: "gpt-4",
+        messages: prompt,
+      })
+      .then((data) => {
+        return { success: true, data: data };
+      })
+      .catch((err) => {
+        return { success: false, error: err };
+      });
+  }
+
+  return { success: false, error: "Text is empty" };
+};
+
+const explainImage = async (imageUrl) => {
+  if (image?.length > 0) {
+    const prompt = {
+      role: "user",
+      content: "explain this image to me" + imageUrl,
+    };
+
+    openai.chat.completions
+      .create({
+        model: "gpt-4",
+        messages: [prompt],
+      })
+      .then((data) => {
+        return { success: true, data: data };
+      })
+      .catch((err) => {
+        return { success: false, error: err };
+      });
+  }
+
+  return { success: false, error: "Unable to get image" };
+};
+
+exports.module = { explainText, explainImage };
