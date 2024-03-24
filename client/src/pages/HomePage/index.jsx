3d5d44b0a2085bf63e69e52aabe5f5a6f089bb39
vikash kushwaha <vikashkumar9669901922@gmail.com>
2024-03-24 14:01:36 +0530

    New Component Added "FreeTrailArea"

--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -6,6 +6,7 @@ import HomePageLearnhtml from "../../components/HomePageLearnhtml";
 import HomePageReadbooklight from "../../components/HomePageReadbooklight";
 import HomePageSituplight from "../../components/HomePageSituplight";
 import HomePageUIDesign from "../../components/HomePageUIDesign";
+import FreeTrialArea from "../../components/FreeTrialArea";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
@@ -1724,7 +1725,7 @@ export default function HomePagePage() {
                   </div>
                 </div>
               </div>
-              <div className="h-[2149px] w-[95%] md:h-auto mt-[150px] relative">
+              <div className="h-[2149px] z-[2] w-[95%] md:h-auto mt-[150px] relative">
                 <div className="w-full">
                   <div className="flex md:flex-col items-center">
                     {/* ========================================================== */}
@@ -2273,61 +2274,7 @@ export default function HomePagePage() {
                   </div>
                 </div>
               </div>
-              <div className="self-stretch h-[519px] mt-[168px] bg-gray-50_01 relative">
-                <div className="flex md:flex-col justify-end items-center w-[62%] bottom-[0.00px] right-[0.00px] m-auto absolute md:relative">
-                  <div className="flex self-end justify-end w-[36%] md:w-full mb-[140px] md:p-5 z-[1]">
-                    <div className="flex justify-end w-full p-[29px] sm:p-5 border-black-900_02 border-2 border-solid rounded-[40px]">
-                      <div className="flex self-end mr-2.5 md:mr-0">
-                        <Text as="p" className="capitalize">
-                          Start Your Free Trial Today
-                        </Text>
-                      </div>
-                    </div>
-                  </div>
-                  <Img
-                    src="images/img_polygon_amber_200.png"
-                    alt="polygon_three"
-                    className="w-[64%] md:w-full ml-[-56px] md:ml-0 object-cover rounded-[69px]"
-                  />
-                </div>
-                <div className="flex md:flex-col justify-center w-full h-max left-[-790px] bottom-0 right-0 top-[-300px] m-auto absolute md:relative">
-                  <div className="flex md:flex-col justify-center items-start md:self-stretch md:p-5 flex-1">
-                    <Img
-                      src="images/img_polygon_deep_purple_a200.png"
-                      alt="polygon_five"
-                      className="w-[47%] md:w-full z-[1] object-cover rounded-[69px]"
-                    />
-                    <Text
-                      size="19xl"
-                      as="p"
-                      className="mt-[138px] ml-[-3px] md:ml-0 capitalize text-center"
-                    >
-                      try Contextify for free
-                    </Text>
-                  </div>
-                  <Img
-                    src="images/img_polygon_deep_orange_300.png"
-                    alt="polygon_seven"
-                    className="w-[32%] md:w-full ml-[-4px] md:ml-0 object-cover rounded-[69px]"
-                  />
-                </div>
-                <div className="flex md:flex-col items-center w-[64%] left-[-400px] top-[-500px] m-auto absolute md:relative">
-                  <Img
-                    src="images/img_polygon_purple_a100.png"
-                    alt="polygon_nine"
-                    className="w-[60%] md:w-full object-cover rounded-[69px]"
-                  />
-                  <Text
-                    size="8xl"
-                    as="p"
-                    className="w-[40%] md:w-full ml-[-87px] md:p-5 md:ml-0 capitalize text-center leading-8"
-                  >
-                    Contexttify is a hub puts information into context at the
-                    click of a button
-                  </Text>
-                </div>
-                <div className="h-[50px] w-[18%] left-[40%] top-[27%] m-auto bg-orange-50_01 absolute" />
-              </div>
+              <FreeTrialArea/>
               <div className="w-full mt-[149px] mx-auto md:p-5 max-w-[1240px]">
                 <div className="flex md:flex-col justify-between items-start gap-5">
                   <div className="flex justify-between items-start w-[29%] md:w-full gap-5">
