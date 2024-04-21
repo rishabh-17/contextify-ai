dd342debfd3cd00f5e7cfb9505cb4509417df1e5
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-04-21 16:28:00 +0530

    login & signup

--- client/src/pages/SignIn/index.jsx
+++ client/src/pages/SignIn/index.jsx
@@ -1,110 +1,178 @@
 import React from "react";
-import { Helmet } from "react-helmet";
-import {  Img } from "./Img";
-import {Button} from './Button';
-import {Text} from './Text';
 import { useNavigate } from "react-router-dom";
-import SignUpComponentSetDivider from "../../components/SignUpComponentSetDivider";
-import SignUpComponentSetProperty1 from "../../components/SignUpComponentSetProperty1";
 
-export default function SignInPage() {
+import axios from "axios";
+export default function LoginPage() {
   const navigate = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
+
+  const [formValidity, setFormValidity] = React.useState({
+    password: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValidity((prevState) => ({
+      ...prevState,
+      [name]: value.length ? "" : "This field is required.",
+    }));
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const isFormValid = () => {
+    const { password, email } = formValidity;
+    return !password && !email;
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post(
+        (import.meta.env.VITE_BACKEND_URL || "") + "/api/user/login",
+        {
+          email,
+          password,
+        }
+      );
+      if (data.error) {
+        setError(data.error);
+        setFormValidity((prevState) => ({
+          ...prevState,
+          email: data.error,
+        }));
+        setIsLoading(false);
+      } else {
+        localStorage.setItem("token", data.token);
+        setIsLoading(false);
+        navigate("/");
+      }
+    } catch (err) {
+      setError(err.message);
+      setIsLoading(false);
+    }
+  }
+
   return (
-    <>
-      <Helmet>
-        <title>Sign Up for Contextify - Create Your Account Today</title>
-        <meta
-          name="description"
-          content="Join Contextify by signing up. Take the first step towards leveraging our digital marketing tools. Easy sign-up with Google or Apple."
-        />
-      </Helmet>
-
-      {/* language selection section */}
-      <div className="flex w-full justify-center bg-gray-100 px-14 pt-[20px] md:p-5">
-        {/* main content section */}
-        <div className="mb-[110px] flex w-[66%] flex-col items-center gap-[31px] md:w-full">
-          {/* header section */}
-          <div className="flex items-center justify-between gap-5 self-stretch sm:flex-col">
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-[60%] p-5 border-t border-b border-l border-r border-gray-100 rounded-tl-lg rounded-tr-lg">
+        <div className="container flex flex-col items-center justify-center px-5 md:px-14 md:py-24">
+          <div className="container flex items-center justify-between gap-2 self-stretch sm:flex-col">
             <div className="flex items-center pl-2">
-              <Text as="p" className="self-end !text-blue_gray-900">
+              <p className="self-end text-blue_gray-900">
                 English (united States)
-              </Text>
-              <Img src="images/img_property_1_arrow.svg" alt="propertyone_one" className="h-[24px] w-[24px]" />
+              </p>
+              <img
+                src="images/img_property_1_arrow.svg"
+                alt="propertyone_one"
+                className="h-[24px] w-[24px]"
+              />
             </div>
-
-            {/* signup prompt section */}
             <div className="flex items-center gap-2">
               <div className="flex">
-                <Text size="s" as="p" className="self-start !text-gray-700 underline">
-                  <span className="text-blue_gray-900">Don’t have an account?</span>
+                <p className="self-end text-gray-700 underline">
+                  <span className="text-blue_gray-900">
+                    Don't have an account?
+                  </span>
                   <span className="text-gray-900_01">&nbsp;&nbsp;</span>
-                </Text>
+                </p>
               </div>
-              <Button
-                size="xs"
-                className="min-w-[131px] rounded-md border border-solid border-gray-900_01 !text-gray-900_01 sm:px-5"
+              <button
+                className="min-w-[131px] rounded-md border border-solid border-gray-900_01 text-gray-900_01 sm:px-5"
                 onClick={() => navigate("/signup")}
               >
-                Sign up
-              </Button>
+                Sign Up
+              </button>
             </div>
           </div>
-          <div className="container-xs flex flex-col items-center gap-[30px] rounded-[20px] border border-solid border-gray-700_59 p-10 md:p-5">
-            {/* login intro section */}
-            <div className="flex w-[73%] flex-col items-center md:w-full">
+
+          <div className="container-xs w-[50%] flex flex-col items-center gap-[10px] rounded-[32px] border border-solid  m-4 border-gray-700 p-5 pt-1 md:p-5">
+            <div className="flex w-[73%] flex-col items-center gap-1 md:w-full">
               <div className="h-[40px] w-[40px] rounded-[20px] bg-gray-400_01" />
-              <Text size="4xl" as="p" className="mt-[7px] !text-blue_gray-900">
-                Login{" "}
-              </Text>
-              <Text as="p" className="text-center !text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis maximus{" "}
-              </Text>
+              <a href="#">
+                <p className="text-blue_gray-900 text-3xl">Login</p>
+              </a>
+              <p className="text-center text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                lobortis maximus{" "}
+              </p>
             </div>
-            <div className="flex flex-col gap-8 self-stretch">
-              {/* login form section */}
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-8 self-stretch"
+            >
               <div className="flex flex-col gap-[23px]">
-                <SignUpComponentSetProperty1 labelOne="Email" className="flex flex-col gap-[5px]" />
-                <SignUpComponentSetProperty1
-                  iconone="images/img_icon.svg"
-                  hidetext="Hide"
-                  className="flex flex-col gap-1"
-                />
-                <Button
-                  color="purple_900"
-                  shape="round"
-                  className="w-full border border-solid border-blue_gray-100 lowercase sm:px-5"
-                  onClick={() => navigate("/")}
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="email" className="text-gray-700">
+                    Email
+                  </label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                    className="self-stretch bg-transparent rounded-xl sm:px-5"
+                  />
+                </div>
+                <div className="flex flex-col gap-[5px]">
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-700">Password</p>
+                    <div
+                      className="cursor-pointer flex items-center justify-center rounded-md"
+                      onClick={() => setPasswordVisible(!passwordVisible)}
+                    >
+                      {passwordVisible ? (
+                        <img
+                          src="images/img_icon_eye_close.svg"
+                          alt="eye_close"
+                          className="h-[16px] w-[16px]"
+                        />
+                      ) : (
+                        <img
+                          src="images/img_icon_eye_open.svg"
+                          alt="eye_open"
+                          className="h-[16px] w-[16px]"
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <input
+                    type={passwordVisible ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                    className="self-stretch sm:px-5  bg-transparent rounded-xl"
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="w-full border border-solid border-blue_gray-100 lowercase bg-purple-900 text-[#fff] hover:bg-[#fff] hover:text-purple-900 py-2 rounded-xl sm:px-5"
+                  disabled={!isFormValid()}
                 >
-                  login
-                </Button>
+                  LOGIN
+                </button>
               </div>
-
-              {/* social login divider section */}
-              {/* <SignUpComponentSetDivider className="flex items-center gap-[23px] sm:flex-col" /> */}
-
-              {/* social login buttons section */}
-              {/* <div className="flex flex-col gap-4">
-                <Button
-                  shape="round"
-                  leftIcon={
-                    <Img src="images/img_social_media_logo.svg" alt="Social media logo" className="h-[24px] w-[24px]" />
-                  }
-                  className="w-full gap-4 border border-solid border-blue_gray-900 sm:px-5"
-                >
-                  Continue with Google
-                </Button>
-                <Button
-                  shape="round"
-                  leftIcon={<Img src="images/img_user_blue_gray_900.svg" alt="user" className="h-[32px] w-[32px]" />}
-                  className="w-full gap-4 border border-solid border-blue_gray-900 sm:px-5"
-                >
-                  Continue with Apple
-                </Button>
-              </div> */}
-            </div>
+            </form>
+            {error && <p className="text-red-500">{error}</p>}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,118 +1,207 @@
 import React from "react";
-import { Helmet } from "react-helmet";
-import {CheckBox} from "../../components";
-import { Button } from "pages/SignIn/Button";
-import { Text } from "pages/SignIn/Text";
-import { Img } from "pages/SignIn/Img";
 import { useNavigate } from "react-router-dom";
-import SignUpComponentSetDivider from "../../components/SignUpComponentSetDivider";
-import SignUpComponentSetProperty1 from "../../components/SignUpComponentSetProperty1";
+import axios from "axios";
 
 export default function SignUpPage() {
   const navigate = useNavigate();
-  return (
-    <>
-      <Helmet>
-        <title>Create Your Contextify Account - Get Started Now</title>
-        <meta
-          name="description"
-          content="Ready to explore Contextify? Create your account in minutes and agree to our Terms of Use and Privacy Policy. Sign up with Google or Apple."
-        />
-      </Helmet>
+  const [username, setUsername] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
 
-      {/* language selection section */}
-      <div className="flex w-full justify-center bg-gray-100 px-14 pt-[20px] md:p-5">
-        <div className="mt-[20px] flex w-[68%] flex-col items-center  md:w-full">
-          {/* login prompt section */}
+  const [formValidity, setFormValidity] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValidity((prevState) => ({
+      ...prevState,
+      [name]: value.length ? "" : "This field is required.",
+    }));
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+      case "username":
+        setUsername(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const isFormValid = () => {
+    const { email, password, username } = formValidity;
+    return !email && !password && !username;
+  };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post(
+        (import.meta.env.VITE_BACKEND_URL || "") + "/api/user/signup",
+        {
+          email,
+          password,
+          username,
+        }
+      );
+      if (data.error) {
+        setError(data.error);
+        setFormValidity((prevState) => ({
+          ...prevState,
+          username: data.error,
+        }));
+        setIsLoading(false);
+      } else {
+        navigate("/");
+      }
+    } catch (err) {
+      setError(err.message);
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-[60%] p-5 border-t border-b border-l border-r border-gray-100 rounded-tl-lg rounded-tr-lg">
+        <div className="container flex flex-col items-center justify-center px-5 md:px-14 md:py-24">
           <div className="flex items-center justify-between gap-2 self-stretch sm:flex-col">
             <div className="flex items-center pl-2">
-              <Text as="p" className="self-end !text-blue_gray-900">
+              <p className="self-end text-blue_gray-900">
                 English (united States)
-              </Text>
-              <Img src="images/img_property_1_arrow.svg" alt="propertyone_one" className="h-[24px] w-[24px]" />
+              </p>
+              <img
+                src="images/img_property_1_arrow.svg"
+                alt="propertyone_one"
+                className="h-[24px] w-[24px]"
+              />
             </div>
             <div className="flex items-center gap-2">
               <div className="flex">
-                <Text size="s" as="p" className="self-end !text-gray-700 underline">
-                  <span className="text-blue_gray-900">Already have an account?</span>
+                <p className="self-end text-gray-700 underline">
+                  <span className="text-blue_gray-900">
+                    Already have an account?
+                  </span>
                   <span className="text-gray-900_01">&nbsp;&nbsp;</span>
-                </Text>
+                </p>
               </div>
-              <Button
-                size="xs"
-                className="min-w-[131px] rounded-md border border-solid border-gray-900_01 !text-gray-900_01 sm:px-5"
+              <button
+                className="min-w-[131px] rounded-md border border-solid border-gray-900_01 text-gray-900_01 sm:px-5"
                 onClick={() => navigate("/signin")}
               >
                 Login
-              </Button>
+              </button>
             </div>
           </div>
 
-          {/* signup form section */}
-          <div className="container-xs flex flex-col items-center gap-[10px] rounded-[32px] border border-solid border-gray-700_59 pt-1 md:p-5">
+          <div className="container-xs w-[50%] flex flex-col items-center gap-[10px] rounded-[32px] border border-solid  m-4 border-gray-700 p-5 pt-1 md:p-5">
             <div className="flex w-[73%] flex-col items-center gap-1 md:w-full">
               <div className="h-[40px] w-[40px] rounded-[20px] bg-gray-400_01" />
               <a href="#">
-                <Text size="4xl" as="p" className="!text-blue_gray-900">
-                  Create an account
-                </Text>
+                <p className="text-blue_gray-900 text-3xl">Create an account</p>
               </a>
-              <Text as="p" className="text-center !text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi lobortis maximus{" "}
-              </Text>
+              <p className="text-center text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                lobortis maximus{" "}
+              </p>
             </div>
-            <div className="flex flex-col gap-8 self-stretch">
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-8 self-stretch"
+            >
               <div className="flex flex-col gap-[23px]">
-                <SignUpComponentSetProperty1 labelOne="Username" className="flex flex-col gap-1" />
-                <SignUpComponentSetProperty1 labelOne="Email" className="flex flex-col gap-[5px]" />
-                <SignUpComponentSetProperty1
-                  iconone="images/img_icon.svg"
-                  hidetext="Hide"
-                  className="flex flex-col gap-1"
-                />
-                <CheckBox
-                  name="property1checkb"
-                  label="By creating an account, I agree to our Terms of use and 
-Privacy Policy "
-                  id="property1checkb"
-                  className="gap-2 py-[7px] pr-[31px] text-left text-base text-blue_gray-900 sm:pr-5"
-                />
-                <Button
-                  color="purple_900"
-                  shape="round"
-                  className="w-full border border-solid border-blue_gray-100 lowercase sm:px-5"
-                  onClick={() => navigate("/")}
+                <div className="flex flex-col gap-1">
+                  <label htmlFor="username" className="text-gray-700">
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
+                    className="self-stretch bg-transparent rounded-xl sm:px-5"
+                  />
+                </div>
+                <div className="flex flex-col gap-[5px]">
+                  <label htmlFor="email" className="text-gray-700">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                    className="self-stretch  bg-transparent rounded-xl sm:px-5"
+                  />
+                </div>
+                <div className="flex flex-col gap-1">
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-700">Password</p>
+                    <div
+                      className="cursor-pointer flex items-center justify-center rounded-md"
+                      onClick={() => setPasswordVisible(!passwordVisible)}
+                    >
+                      {passwordVisible ? (
+                        <img
+                          src="images/img_icon_eye_close.svg"
+                          alt="eye_close"
+                          className="h-[16px] w-[16px]"
+                        />
+                      ) : (
+                        <img
+                          src="images/img_icon_eye_open.svg"
+                          alt="eye_open"
+                          className="h-[16px] w-[16px]"
+                        />
+                      )}
+                    </div>
+                  </div>
+                  <input
+                    type={passwordVisible ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={handleChange}
+                    className="self-stretch sm:px-5  bg-transparent rounded-xl"
+                  />
+                </div>
+                <div className="flex items-center gap-2 text-left text-base text-blue_gray-900 sm:pr-5">
+                  <input
+                    type="checkbox"
+                    id="property1checkb"
+                    name="property1checkb"
+                    className="mr-[1px] "
+                  />
+                  <label htmlFor="property1checkb">
+                    By creating an account, I agree to our Terms of use and
+                    Privacy Policy
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full border border-solid border-blue_gray-100 lowercase bg-purple-900 text-[#fff] hover:bg-[#fff] hover:text-purple-900 py-2 rounded-xl sm:px-5"
+                  disabled={!isFormValid()}
                 >
-                  cREATE AN ACCOUNT
-                </Button>
+                  CREATE AN ACCOUNT
+                </button>
               </div>
-
-              {/* social media divider section */}
-              {/* <SignUpComponentSetDivider className="flex items-center gap-[23px] sm:flex-col" /> */}
-
-              {/* social media buttons section */}
-              {/* <div className="flex flex-col gap-4">
-                <Button
-                  shape="round"
-                  leftIcon={
-                    <Img src="images/img_social_media_logo.svg" alt="Social media logo" className="h-[24px] w-[24px]" />
-                  }
-                  className="w-full gap-4 border border-solid border-blue_gray-900 sm:px-5"
-                >
-                  Continue with Google
-                </Button>
-                <Button
-                  shape="round"
-                  leftIcon={<Img src="images/img_user_blue_gray_900.svg" alt="user" className="h-[32px] w-[32px]" />}
-                  className="w-full gap-4 border border-solid border-blue_gray-900 sm:px-5"
-                >
-                  Continue with Apple
-                </Button>
-              </div> */}
-            </div>
+            </form>
+            {error && <p className="text-red-500">{error}</p>}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
