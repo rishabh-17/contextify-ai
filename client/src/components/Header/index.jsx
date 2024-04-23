b3286010bd2536beebb2277e592d584355963faa
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-04-23 22:32:57 +0530

    getting started route update

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -64,7 +64,7 @@ export default function Header({ ...props }) {
                   if (localStorage.getItem("token")) {
                     navigate("/dashboard");
                   } else {
-                    navigate("/login");
+                    navigate("/signin");
                   }
                 }}
               >
