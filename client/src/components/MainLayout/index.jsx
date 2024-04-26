5b9a92c784290a52c579714596c5d52a72604497
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-04-26 09:09:40 +0000

    dropdown navigation

--- client/src/components/MainLayout/index.jsx
+++ client/src/components/MainLayout/index.jsx
@@ -97,26 +97,26 @@ export default function ClientdashboardPage({ active, children }) {
                   className="h-6 shadow-xl"
                 />
         </div>
-        <div>
+        <a onClick={()=>navigate('/profile')}>
           <p>Profile</p>
-          <div className="font-medium truncate">name@flowbite.com</div>
-        </div>
+          <div className="font-medium truncate">name@contextify.com</div>
+        </a>
 
         </div>
         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
-          <li>
-            <a href="#" className="px-4 py-2 flex flex-row gap-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+          <li onClick={()=>navigate('/dashboard')}>
+            <a className="px-4 py-2 flex flex-row gap-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
             <MdOutlineSpaceDashboard className="h-4 w-4"/>
               Dashboard
             </a>
           </li>
-          <li>
-            <a href="#" className="px-4 py-2 flex flex-row gap-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+          <li onClick={()=>navigate('/subscription')}>
+            <a  className="px-4 py-2 flex flex-row gap-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
             <IoPowerSharp className="h-4 w-4"/>
               Subscription
             </a>
           </li>
-          <li>
+          <li onClick={()=>navigate('/support')}>
             <a href="#" className=" px-4 py-2 flex flex-row gap-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
             <MdOutlineContactSupport className="h-4 w-4"/>
               Support
