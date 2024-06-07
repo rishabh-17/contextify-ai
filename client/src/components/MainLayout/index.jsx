0f0fbf5c3438f1f0c14f0bd011d9cb4044f8f00a
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-06-08 00:01:57 +0530

    setting

--- client/src/components/MainLayout/index.jsx
+++ client/src/components/MainLayout/index.jsx
@@ -296,7 +296,7 @@ export default function ClientdashboardPage({ active, children }) {
                                 Support
                               </a>
                             </li>
-                            <li onClick={() => navigate("/support")}>
+                            <li onClick={() => {}}>
                               <a
                                 href="#"
                                 className=" px-4 py-2 flex flex-row gap-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white text-purple-900"
