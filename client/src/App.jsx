ae8140ef0c5aa7cda32fd24793920125dbae740e
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-06-12 22:55:05 +0530

    auth updaate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,21 @@ import React, { useState, createContext } from "react";
 import Routes from "./Routes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import Loader from "./components/Loader"
+import Loader from "./components/Loader";
 
-export const LoadingContext = createContext()
+export const LoadingContext = createContext();
 
 function App() {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   return (
-    <GoogleOAuthProvider clientId="355279814554-it9r93p3kcd9565hj425ahdse2o7k42v.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="373354593732-tn4ij8vgjhloghl1bt2l6mktmn2t7jei.apps.googleusercontent.com">
       <LoadingContext.Provider value={setLoading}>
-      {loading && <Loader />}
-      <div className={loading?"h-screen overflow-hidden":""}>
-      <Router>
-        <Routes />
-      </Router>
-      </div>
+        {loading && <Loader />}
+        <div className={loading ? "h-screen overflow-hidden" : ""}>
+          <Router>
+            <Routes />
+          </Router>
+        </div>
       </LoadingContext.Provider>
     </GoogleOAuthProvider>
   );
