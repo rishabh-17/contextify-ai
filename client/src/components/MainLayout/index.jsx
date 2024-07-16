4beb8f343d2f9e356001c85598b58d47f1010979
Hrishabh Tiwari <rishabh2560@gmail.com>
2024-07-16 21:43:24 +0530

    menu hidden

--- client/src/components/MainLayout/index.jsx
+++ client/src/components/MainLayout/index.jsx
@@ -253,7 +253,7 @@ export default function ClientdashboardPage({ active, children }) {
                   <div>
                     <button
                       id="dropdownInformationButton"
-                      className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-blue-700"
+                      className="text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center  hidden sm:inline-flex  dark:hover:bg-blue-700"
                       type="button"
                       onClick={toggleDropdown}
                     >
